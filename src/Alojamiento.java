6a2792b598d97c1bb83a1f52fd75f81e50a9f94b
antivsan796 <antivsan@espol.edu.ec>
2024-11-08 22:46:03 -0500

    Clases
    puse la clase alojamiento y clase habitacion

--- src/Alojamiento.java
+++ src/Alojamiento.java
@@ -1,3 +1,9 @@
-public class Alojamiento {
-    
+public abstract class Alojamiento {
+    protected String id;
+    protected String tipo; // Puede ser "Habitación", "Departamento", etc.
+    protected String estado; // "Disponible", "Reservado", etc.
+    protected double precioPorNoche;
+
+    public abstract void reservar();
+
 }
