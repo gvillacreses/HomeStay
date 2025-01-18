34446e4eaefbc768dff5cf5cfd4f3161778be4ca
jorssanc <jorssanc@espol.edu.ec>
2025-01-17 22:11:52 -0500

    Eliminar clase Subscripcion, no aplica en Observer

--- src/Subscripcion.java
+++
@@ -1,6 +0,0 @@
-
-public interface Subscripcion{
-    void addSubscriptor(Subscriptor subscriptor);
-    void removeSubscriptor(Subscriptor subscriptor);
-    void notificarSubscriptor(String mensaje);
-}
